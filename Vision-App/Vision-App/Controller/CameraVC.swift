6c1a348860092c4b09d170b93365049e741e1c2f
Mitchell Schuster <mgschuster11@gmail.com>
2017-09-18 08:42:50 -0500

    Add files via upload

---
+++ Vision-App/Vision-App/Controller/CameraVC.swift
@@ -0,0 +1,17 @@
+//
+//  CameraVC.Swift
+//  Vision-App
+//
+//  Created by Admin on 9/18/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
